1b5eaf5929d83f81b4e260a7dd6d6f3e7db3b830
yinmm <yinmanmanno@gmail.com>
2024-06-13 17:32:59 +0800

    copywriter

--- web/app/components/base/logo/logo-site.tsx
+++ web/app/components/base/logo/logo-site.tsx
@@ -11,7 +11,7 @@ const LogoSite: FC<LogoSiteProps> = ({
   return (
     <img
       src='/logo/logo-site.svg'
-      className={classNames('block w-auto h-8', className)}
+      className={classNames('block w-auto h-6', className)}
       alt='logo'
     />
   )
